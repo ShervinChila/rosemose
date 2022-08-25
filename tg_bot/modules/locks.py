80975e4f48e408ab4743dc0d2656e2c3d933acd7
ShervinChila <108692453+ShervinChila@users.noreply.github.com>
2022-08-25 05:41:22 +0430

    Update locks.py

--- tg_bot/modules/locks.py
+++ tg_bot/modules/locks.py
@@ -18,7 +18,7 @@ from tg_bot.modules.helper_funcs.filters import CustomFilters
 from tg_bot.modules.log_channel import loggable
 from tg_bot.modules.sql import users_sql
 
-LOCK_TYPES = {'استیکر': Filters.sticker,
+LOCK_TYPES = {'sticker': Filters.sticker,
               '🎧': Filters.audio,
               ['ok', 'voice']: Filters.voice,
               'اسناد': Filters.document,
